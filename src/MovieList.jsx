1f38aa059e032f4c0a4e6ae075bd19d7b21034a4
His Eminence <usaske279@gmail.com>
2025-09-08 00:45:44 +0300

    gg

---
+++ src/MovieList.jsx
@@ -0,0 +1,12 @@
+
+
+const MovieList = (props) => {
+    return (
+        <div className="text-white">
+            {props.movieList.map((movie, index)=>{
+                return <div key={index}>{movie}</div>
+            })}
+        </div>
+    )
+}
+export default MovieList;
